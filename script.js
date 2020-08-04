e57234d332c5745f2919049e20b69f1883998116
Gianluca Galota <gianluca.galota@gmail.com>
2020-08-04 18:18:29 +0100

    project almost over,need to implement input box for grid resize and reset option keeping the last grid row setting

--- script.js
+++ script.js
@@ -1,80 +1,100 @@
 //Caching the DOM
 const container = document.getElementById("container");
 const gridItem = document.getElementsByClassName("grid-item");
-const button = document.getElementById("grid-size");
-
-
-
-// Create Grid 
-function makeGrid(rows, cols){
-    container.style.setProperty("--grid-rows", rows);
-    container.style.setProperty("--grid-cols", cols);
-    container.style.setProperty("height", "800px");
-    container.style.setProperty("width", "600px");
-    for (i = 0; i < (rows * cols); i++) {
-        let cell = document.createElement("div");
-        
-        container.appendChild(cell).className = "grid-item";
-    };
-
-};
-
-// Call Function Make Grid
-makeGrid(16, 16);
-
-
-
-button.addEventListener("click", function(){
-    var size = prompt("Which grid size would you like?");
-    var num1 = parseInt(size);
-    var num2 = parseInt(size);
-    makeGrid(num1,num2);
-    for(let i=0; i<gridItem.length; i++){
-    gridItem[i].addEventListener("mouseenter", function (){
-        gridItem[i].style.backgroundColor = "blue";
-       
-    })
-}
-   
-    
-})
-
-// Hover Effect
-for(let i=0; i<gridItem.length; i++){
-    gridItem[i].addEventListener("mouseenter", function (){
-        gridItem[i].style.backgroundColor = "blue";
-       
-    })
-}
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
+const buttonGrid = document.getElementById("grid-size");
+const buttonRandom = document.getElementById("random-colors");
+const buttonColorPicker = document.getElementById("color-picker");
+const buttonGridLines = document.getElementById("grid-lines");
+const buttonErase = document.getElementById("erase");
+const colorValue = document.getElementById("color-value");
  
 
 
 
+// Create Grid 
+function makeGrid(rows, cols) {
+    container.style.setProperty("--grid-rows", rows);
+    container.style.setProperty("--grid-cols", cols);
+    for (i = 0; i < (rows * cols); i++) {
+        let cell = document.createElement("div");
+        container.appendChild(cell).className = "grid-item";
+
+    };
+
+    colorHoverEffect();
+
+};
+
+
+// Call Function Make Grid
+makeGrid(32, 32);
+
+// Reset Grid Every Time User Give New Size
+function resetGrid() {
+    container.textContent = "";
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+
+// Change Grid Size Event
+buttonGrid.addEventListener("click", function () {
+    resetGrid();
+    var size = prompt("Choose grid size");
+    var num1 = parseInt(size);
+    var num2 = parseInt(size);
+    makeGrid(num1, num2);
+})
+
+// Generate Random Color
+function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+// Random Color Hover Effect
+function randomHoverEffect() {
+    for (let i = 0; i < gridItem.length; i++) {
+        gridItem[i].addEventListener("mouseover", function () {
+            gridItem[i].style.backgroundColor = getRandomColor();
+
+        })
+
+    }
+}
+
+// Color Picker Hover Effect
+function colorHoverEffect() {
+    for (let i = 0; i < gridItem.length; i++) {
+        gridItem[i].addEventListener("mouseover", function () {
+            gridItem[i].style.backgroundColor = buttonColorPicker.value;
+
+        })
+
+    }
+    colorValue.style.color = buttonColorPicker.value;
+    colorValue.textContent = buttonColorPicker.value;
+}
+
+// Grid Lines On-Off
+function gridLines() {
+    for (let i = 0; i < gridItem.length; i++) {
+        gridItem[i].classList.toggle("grid-lines");
+
+    }
+}
 
 
 
-
-
-
-
-
+buttonRandom.addEventListener("click", randomHoverEffect);
+buttonColorPicker.addEventListener("input", colorHoverEffect);
+buttonGridLines.addEventListener("click", gridLines);
+buttonErase.addEventListener("click", function(){
+    window.location.reload();
+});
